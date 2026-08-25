4c62f6ebc88590a066aab1034d6671fd851e340a
Bright Mahonga <brightmahonga7@gmail.com>
2026-08-25 21:43:27 +0300

    modified:   backend/models/vehicle.py

--- backend/models/vehicle.py
+++ backend/models/vehicle.py
@@ -7,8 +7,7 @@ class Vehicle(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	plate_number = db.Column(db.String(20), unique=True, nullable=False)
 	vehicle_type = db.Column(db.String(20), nullable=False)
-	owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-	driver_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+	fleet_owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 	is_active = db.Column(db.Boolean, nullable=False, default=True)
 
 	remittances = db.relationship("Remittance", back_populates="vehicle", lazy=True)
