e74975f55b362774330117e3ccc6db5345acf0d9
Sam Tomashi <samtomashikayembi@yhaoo.fr>
2026-09-02 16:58:57 +0300

    implement JWT implementation

--- backend/config.py
+++ backend/config.py
@@ -6,3 +6,8 @@ class Config:
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "development-only-secret")
 	MPESA_CALLBACK_SECRET = os.getenv("MPESA_CALLBACK_SECRET", "")
+	JWT_TOKEN_LOCATION = ['cookies']
+	JWT_ACCESS_COOKIE_NAME = 'access_token_cookie'
+	JWT_COOKIE_CSRF_PROTECT = False
+	# JWT_COOKIE_HTTPONLY = True
+
